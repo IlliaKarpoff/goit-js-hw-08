df727b417aba6b7a470a631969984cd8918ecdba
Illia Karpoff <hawaii2001@ukr.net>
2020-09-06 13:09:58 +0300

    added karusel

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,9 +1,11 @@
 export default [
     {
       preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+        // 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg',
       original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+        // 'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg',
       description: 'Hokkaido Flower',
     },
     {
@@ -15,9 +17,11 @@ export default [
     },
     {
       preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg',
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+        // 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg',
       original:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg',
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+        // 'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg',
       description: 'Aerial Beach View',
     },
     {
--- js/index.js
+++ js/index.js
@@ -1,32 +1,34 @@
 import images from './gallery-items.js';
 
+let currentId = 0;
 const ulRef = document.querySelector('.js-gallery');
 const overlayRef = document.querySelector('.lightbox__content');
 const overRef = document.querySelector('.lightbox');
 const imgOverRef = document.querySelector('.lightbox__image');
 const closeBtn = overRef.querySelector('button[data-action="close-lightbox"]');
 
-const createLi = image => {
+const createLi = (image, index) => {
   const ref = {
       ul: document.querySelector('.js-gallery'),
       li: document.createElement('li'),
       a: document.createElement('a'),
       img: document.createElement('img'),
-      // img: document.createElement('img[data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"]'),
   }
   ref.li.classList.add('gallery__item');
   ref.a.classList.add('gallery__link');
   ref.a.href = image.original;
   ref.img.classList.add('gallery__image');
   ref.img.src = image.preview;
-  ref.img.alt = image.original;
-  ref.img.description = image.description;
-  // const data = data-source;                     // ???   // 'button[data-action="render"]'
+  ref.img.setAttribute('data-source', image.original);
+  ref.img.setAttribute('data-index', index);
+  ref.img.alt = image.description;
   ref.a.appendChild(ref.img);
   ref.li.appendChild(ref.a);
+  console.log(ref.li);
   return ref.li;
 };
-const arrLi = images.map(image => createLi(image));
+
+const arrLi = images.map((image, index) => createLi(image, index));
 ulRef.append(...arrLi);
 
 const openModal = () => {
@@ -35,20 +37,43 @@ const openModal = () => {
     return;
   }
   overRef.classList.add('is-open');
-  imgOverRef.src = event.target.alt;
-  window.addEventListener('keydown', escCloseModal); // - чи є різниця в якому місці зареєстрований слухач? 
-  window.addEventListener('keydown', onNextImage);   // - куди краще повісити? чи треба знімати?
+  imgOverRef.src = event.target.dataset.source;
+  currentId = Number(event.target.dataset.index);
+  console.log(currentId);
+  window.addEventListener('keydown', onKeyPress);
 }
 const closeModal = () => {
   overRef.classList.remove('is-open');
   imgOverRef.src = '';
-  window.removeEventListener('keydown', escCloseModal);
+  window.removeEventListener('keydown', onKeyPress);
 }
-const escCloseModal = event => {
+const onKeyPress = event => {
   if (event.code === 'Escape') {
     closeModal();
   }
-}                                   // - чому після Ескейпа доводиться клікати на імедж двічі?
+  if (event.code === 'ArrowLeft') {
+    if (currentId > 0 && currentId <= images.length-1) {
+      currentId -= 1;
+      console.log(currentId);
+      imgOverRef.src = images[currentId].original;
+    } else if (currentId === 0) {
+      currentId = images.length-1;
+      console.log(currentId);
+      imgOverRef.src = images[currentId].original;
+    }
+  }
+  if (event.code === 'ArrowRight') {
+    if (currentId >= 0 && currentId < images.length-1) {
+      currentId += 1;
+      console.log(currentId);
+      imgOverRef.src = images[currentId].original;
+    } else if (currentId = images.length-1) {
+      currentId = 0;
+      console.log(currentId);
+      imgOverRef.src = images[currentId].original;
+    }
+  }
+}                                                  // - чому після Ескейпа доводиться клікати на імедж двічі?
 const onBackClick = event => {
   if (event.target === event.currentTarget) {
     closeModal();
@@ -58,19 +83,3 @@ const onBackClick = event => {
 ulRef.addEventListener('click', openModal)
 closeBtn.addEventListener('click', closeModal);
 overlayRef.addEventListener('click', onBackClick);
-
-// Пролистывание изображений галереи в открытом модальном окне 
-// клавишами "влево" и "вправо".
-
-// const onNextImage = event => {
-//   console.log(event.code);
-//   for (let i=0; i<images.length; ) {
-
-//     if (event.code === 'ArrowLeft') {
-//       imgOverRef.src = images[i-1].alt;
-//       // if (event.code === 'ArrowRight') {
-//         // imgOverRef.src = images[i+1].alt;
-//         //   closeModal();
-//       }
-//   }
-// }
